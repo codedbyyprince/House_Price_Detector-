5ccce29ea8c0edafee99c45382252c7f9838a354
prince <princenagda35@gmail.com>
2025-09-12 16:39:39 +0530

    training the data with diffrent models to test which one to select

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
 
 # 1. reading the data
 housing = pd.read_csv('housing.csv')
@@ -51,4 +55,29 @@ full_pipline = ColumnTransformer([
 
 # 6. transform the data
 housing_prepared = full_pipline.fit_transform(housing)
-print(housing_prepared)
+# print(housing_prepared)
+
+
+# 7. train the model 
+
+# linear regresion 
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared , housing_labels)
+lin_preds = lin_reg.predict(housing_prepared)
+lin_rmse = root_mean_squared_error(housing_labels , lin_preds)
+print(f"linearregresion = {lin_rmse}")
+
+# decison tree  regresion 
+dec_reg = DecisionTreeRegressor()
+dec_reg.fit(housing_prepared , housing_labels)
+dec_preds = dec_reg.predict(housing_prepared)
+dec_rmse = root_mean_squared_error(housing_labels , dec_preds)
+print(f"decisiontree = {dec_rmse}")
+
+# random forest regresion 
+ran_reg = RandomForestRegressor()
+ran_reg.fit(housing_prepared , housing_labels)
+ran_preds = ran_reg.predict(housing_prepared)
+ran_rmse = root_mean_squared_error(housing_labels , ran_preds)
+print(f"randomforest = {ran_rmse}")
+
