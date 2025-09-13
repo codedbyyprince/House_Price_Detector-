d07e7fac04e7d034ebb2f914ce546aadf55647a4
prince <princenagda35@gmail.com>
2025-09-13 15:15:42 +0530

    model is trained

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import joblib
+import os 
 import pandas as pd
 import numpy as np 
 from sklearn.model_selection import StratifiedShuffleSplit
@@ -11,105 +13,54 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
 from sklearn.model_selection import cross_val_score
 
-# 1. reading the data
-housing = pd.read_csv('housing.csv')
+MODEL_FILE = "model.pkl"
+PIPLINE_FILE = "pipline.pkl"
 
-#2. creating a startified test set 
-housing['income_cat'] = pd.cut(housing['median_income'], 
-                               bins = [0.0, 1.5, 3.0, 4.5, 6.0,  np.inf],
-                               labels = [1,2,3,4,5])
-
-split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-
-for train_index , test_index in split.split(housing, housing['income_cat']):
-    strat_train = housing.loc[train_index].drop("income_cat", axis=1)
-    strat_test = housing.loc[test_index].drop("income_cat", axis=1)
-
-
-housing = strat_train.copy()
-
-#3. seprating labels and features
-housing_labels = housing['median_house_value'].copy()
-housing = housing.drop("median_house_value", axis=1)
-
-# print(housing, housing_labels)
-
-# 4 seprte numerical and categorial columns 
-num_attribs = housing.drop('ocean_proximity', axis=1).columns.tolist()
-cat_atribs = ["ocean_proximity"]
-
-# lets make the pipline 
-num_pipline = Pipeline([
-    ("imputer" , SimpleImputer(strategy="median")),
-    ("scaler" , StandardScaler())
-])
-
-cat_pipline = Pipeline([
-    ("onehot" , OneHotEncoder(handle_unknown="ignore"))
-])
-
-# constructing full pipline 
-full_pipline = ColumnTransformer([
-    ("num" , num_pipline, num_attribs),
-    ("cat", cat_pipline, cat_atribs)    
+def build_pipline(num_attribs, cat_atribs):
+    # lets make the pipline 
+    num_pipline = Pipeline([
+        ("imputer" , SimpleImputer(strategy="median")),
+        ("scaler" , StandardScaler())
     ])
 
-# 6. transform the data
-housing_prepared = full_pipline.fit_transform(housing)
-# print(housing_prepared)
+    cat_pipline = Pipeline([
+        ("onehot" , OneHotEncoder(handle_unknown="ignore"))
+    ])
 
+    # constructing full pipline 
+    full_pipline = ColumnTransformer([
+        ("num" , num_pipline, num_attribs),
+        ("cat", cat_pipline, cat_atribs)    
+        ])
+    return full_pipline
 
-# 7. train the model 
+if not os.path.exists(MODEL_FILE):
+    # training the model 
+    housing = pd.read_csv('housing.csv')
 
-# linear regresion 
-lin_reg = LinearRegression()
-lin_reg.fit(housing_prepared , housing_labels)
-lin_preds = lin_reg.predict(housing_prepared)
-# lin_rmse = root_mean_squared_error(housing_labels , lin_preds)
-# print(f"linearregresion = {lin_rmse}")
-lin_rmses = -cross_val_score(
-    lin_reg,
-    housing_prepared,
-    housing_labels,
-    scoring="neg_root_mean_squared_error",
-    cv=10
-)
+    #2. creating a startified test set 
+    housing['income_cat'] = pd.cut(housing['median_income'], 
+                                bins = [0.0, 1.5, 3.0, 4.5, 6.0,  np.inf],
+                                labels = [1,2,3,4,5])
 
-print(pd.Series(lin_rmses).describe())
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 
+    for train_index , test_index in split.split(housing, housing['income_cat']):
+        housing = housing.loc[train_index].drop("income_cat", axis=1)
 
+    housing_labels = housing['median_house_value'].copy()
+    housing_features = housing.drop("median_house_value", axis=1)
 
-# decison tree  regresion 
-dec_reg = DecisionTreeRegressor()
-dec_reg.fit(housing_prepared , housing_labels)
-dec_preds = dec_reg.predict(housing_prepared)
-# dec_rmse = root_mean_squared_error(housing_labels , dec_preds)
-# print(f"decisiontree = {dec_rmse}")
-dec_rmses = -cross_val_score(
-    dec_reg,
-    housing_prepared,
-    housing_labels,
-    scoring="neg_root_mean_squared_error",
-    cv=10
-)
+    num_attribs = housing_features.drop('ocean_proximity', axis=1).columns.tolist()
+    cat_atribs = ["ocean_proximity"]
 
-print(pd.Series(dec_rmses).describe())
+    pipline = build_pipline(num_attribs, cat_atribs)
+    housing_prepared = pipline.fit_transform(housing_features)
 
+    model = RandomForestRegressor()
+    model.fit(housing_prepared, housing_labels)
 
-# random forest regresion 
-ran_reg = RandomForestRegressor()
-ran_reg.fit(housing_prepared , housing_labels)
-ran_preds = ran_reg.predict(housing_prepared)
-# ran_rmse = root_mean_squared_error(housing_labels , ran_preds)
-# print(f"randomforest = {ran_rmse}")
-ran_rmses = -cross_val_score(
-    ran_reg,
-    housing_prepared,
-    housing_labels,
-    scoring="neg_root_mean_squared_error",
-    cv=10
-)
+    joblib.dump(model , MODEL_FILE)
+    joblib.dump(pipline , PIPLINE_FILE)
 
-print(pd.Series(ran_rmses).describe())
-
-
+    print("CONGRATS! MODEL IS TRAINED ")
